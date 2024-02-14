aab494b3a49fa0fce7265fbc7870f1a1247af149
Douglas Panhota <douglaspands@gmail.com>
2024-02-14 00:15:10 -0300

    ajustes gerais na geracao do template

--- www/webpack.common.js
+++ www/webpack.common.js
@@ -3,20 +3,21 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const glob = require('glob');
 
 const FOLDER_JS = "src/js";
-const FOLDER_SASS = "src/scss";
+const FOLDER_SCSS = "src/scss";
 const FOLDER_DIST = "static";
 
 module.exports = {
   entry: (() => {
     const entries = Object.assign({},
       getDynamicEntries(`./${FOLDER_JS}/**/*.js`),
-      getDynamicEntries(`./${FOLDER_SASS}/**/*.scss`)
+      getDynamicEntries(`./${FOLDER_SCSS}/**/*.scss`)
     )
     return entries;
   })(),
   output: {
     path: path.resolve(__dirname, FOLDER_DIST),
-    filename: '[name].bundle.js',
+    // filename: '[name].bundle.js',
+    filename: '[name].js',
   },
   resolve: {
     extensions: ['.js', '.json'],
