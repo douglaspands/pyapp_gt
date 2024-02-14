c9e1036c94138f0eb81c290efcdd51d7f0c9085b
Douglas Panhota <douglas.panhota@ape11.com.br>
2024-02-14 12:43:57 -0300

    inclusao de pasta no www para testes

--- django_core/settings/base.py
+++ django_core/settings/base.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "django_core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR.parent / "db.sqlite3",
     }
 }
 
