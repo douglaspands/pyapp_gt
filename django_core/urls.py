214dc492851703f692663d7eff74d0fb4547c343
Douglas Panhota <douglaspands@gmail.com>
2024-02-13 23:58:24 -0300

    inclusao do app www

--- django_core/urls.py
+++ django_core/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("/", include("www.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("www.urls"))]
 
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
--- www/urls.py
+++ www/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from www.views.home import HomeView
 
 urlpatterns = [
-    path("/", HomeView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="home"),
 ]
