3a0e6f2f6354755ccc8f8003ced1d4c512e9817f
Douglas Panhota <douglaspands@gmail.com>
2024-03-10 23:02:45 -0300

    remocao do browser sync

--- www/webpack.dev.js
+++ www/webpack.dev.js
@@ -1,21 +1,11 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-
-commonConfig.plugins.push(
-  new BrowserSyncPlugin({
-    open: true,
-    host: 'localhost',
-    port: 3000,
-    proxy: "http://127.0.0.1:8000"
-  })
-);
 
 module.exports = merge(commonConfig, {
   mode: 'development',
-  watch: true,
   devtool: 'inline-source-map',
+  watch: true,
   watchOptions: {
-    poll: true,
+    ignored: ['**/node_modules'],
   },
 });
